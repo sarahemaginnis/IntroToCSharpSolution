133a3d45be9d2cc34d4eb183fe598f061e2ec038
sarahemaginnis <sarahemaginnis@gmail.com>
2022-07-12 21:01:32 -0500

    Add project files.

---
+++ IntroToCSharpProject/Program.cs
@@ -0,0 +1,37 @@
+﻿//pick a number between 1 and 100
+
+//type variable name           Classes need to be instantiated with the 'new' keyword
+Random randomNumberGenerator = new Random();
+int numberToGuess = randomNumberGenerator.Next(1, 101);
+
+Console.Write("Pick a number between 1 and 100: ");
+string input = Console.ReadLine();
+int inputAsInt = int.Parse(input);
+
+bool correctGuess = false;
+if (numberToGuess == inputAsInt) 
+{ 
+    correctGuess = true; 
+}
+
+//ternary; different way of writing an if statement
+//ternary: if statement comes first, then true then false
+string responseText = correctGuess 
+    ? $"That's right, the number I was thinking of was {numberToGuess}!" 
+    : $"Wow, you're bad at this. I was thinking of {numberToGuess} but you guessed {input}.";
+
+//string correctResponse = $"That's right, the number I was thinking of was {numberToGuess}!";
+//string incorrectResponse = $"Wow, you're bad at this. I was thinking of {numberToGuess} but you guessed {input}.";
+
+Console.WriteLine(responseText);
+
+//if (correctGuess)
+//{
+//    Console.WriteLine(correctResponse);
+//}
+//else
+//{
+//    Console.WriteLine(incorrectResponse);
+//}
+
+Console.ReadLine();
